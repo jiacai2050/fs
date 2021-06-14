dab7094bf157db39c2b3c00165a931ca456f0796
jiacai2050 <dev@liujiacai.net>
2021-06-14 17:44:32 +0800

    bump clojure version, add github actions

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject me.raynes/fs "1.4.6"
+(defproject org.clojars.jiacai/fs "1.4.6"
   :description "File system utilities for clojure"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :url "https://github.com/Raynes/fs"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :url "https://github.com/jiacai2050/fs"
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.apache.commons/commons-compress "1.8"]]
-  :plugins [[lein-midje "3.1.3"]
+  :plugins [[lein-midje "3.2.1"]
             [codox "0.8.10"]]
   :codox {:src-dir-uri "https://github.com/Raynes/fs/blob/master/"
           :src-linenum-anchor-prefix "L"
           :defaults {:doc/format :markdown}}
   :deploy-repositories {"releases" :clojars}
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
+  :profiles {:dev {:dependencies [[midje "1.10.3"]]}})
